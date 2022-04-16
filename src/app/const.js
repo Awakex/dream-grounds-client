8994f68fb35a359f5ebb587056d418db597647f3
Igor <awkworkx@gmail.com>
2022-04-16 14:26:32 +0300

    upload4

--- src/app/const.js
+++ src/app/const.js
@@ -1,6 +1,6 @@
-export const WebsocketSettings = {
-    URL: "http://localhost:3000",
-};
 // export const WebsocketSettings = {
-//     URL: "188.187.148.35:3000",
+//     URL: "http://localhost:3000",
 // };
+export const WebsocketSettings = {
+    URL: "http://188.187.148.35:3000",
+};
